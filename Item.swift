8257824d9ea8a5c2ba3392ee29bc54a6b7e27d9e
Randall Mardus <randalljmardus@gmail.com>
2016-04-28 14:47:32 -0400

    setup item class

---
+++ Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Randall Mardus on 4/28/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: NSDate
+}
