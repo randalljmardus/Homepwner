a5a1c0d7189c9add2d78e801f928ca2e1c7e926a
Randall Mardus <randalljmardus@gmail.com>
2016-04-29 11:17:30 -0400

    setup dequeue with reusable cell with identifier

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -17,8 +17,9 @@ class ItemsViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        //create an instance of UITableViewCell, with default appearance
-        let cell = UITableViewCell(style: .Value1, reuseIdentifier: "UITableViewCell")
+        
+        //get a new or recycled cell
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
         
         //set the text on the cell with the description of the item
         //that is at the nth index of items, where n = row this cell
