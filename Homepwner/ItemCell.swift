fb49d6660abc85293bf005cfc7579712ca7f682c
Randall Mardus <randalljmardus@gmail.com>
2016-05-03 14:31:24 -0400

    made cell heights dynamic

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Randall Mardus on 5/3/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
