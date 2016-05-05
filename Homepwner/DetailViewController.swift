b33248fbca2a6694d9490e8c8cc56b3c16710ffa
Randall Mardus <randalljmardus@gmail.com>
2016-05-05 13:20:43 -0400

    added uiimageview to detail vc

--- Homepwner/DetailViewController.swift
+++ Homepwner/DetailViewController.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, UITextFieldDelegate {
-    
-    @IBAction func backgroundTapped(sender: UITapGestureRecognizer) {
-        view.endEditing(true)
-    }
+class DetailViewController: UIViewController {
     
     @IBOutlet var nameField: UITextField!
     @IBOutlet var serialNumberField: UITextField!
     @IBOutlet var valueField: UITextField!
     @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBAction func backgroundTapped(sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
     
     var item: Item! {
         didSet {
